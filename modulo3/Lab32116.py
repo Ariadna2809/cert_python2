ad1a8496de066118e5b2f2a2c5aa4229e214f9d8
Ariadna2809 <1219100450@alumnos.utng.edu.mx>
2022-07-30 15:27:27 -0500

    Laboratorio Completado

--- modulo3/Lab32116.py
+++ modulo3/Lab32116.py
@@ -1,2 +1,36 @@
 #Laboratorio 3.2.1.16 Colas alias FIFO parte 2
 #Elaborado por: Ariadna Loredo Estrada
+
+class QueueError(IndexError):
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def put(self, elem):
+        self.queue.insert(0, elem)
+
+    def get(self):
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            raise QueueError 
+
+
+class SuperQueue(Queue):
+    def isempty(self):
+        return len(self.queue) == 0
+
+que = SuperQueue()
+que.put(1)
+que.put("perro")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Cola vacía")
---
+++ modulo3/Lab34112.py
@@ -0,0 +1,22 @@
+#Laboratorio 3.4.1.12 La clase Timer
+#Elaborado por: Ariadna Loredo Estrada
+
+class Timer:
+    def __init__(timer):        
+        return
+
+    def __str__(self):
+        return
+
+    def next_second(self):
+        return
+
+    def prev_second(self):
+        return
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
